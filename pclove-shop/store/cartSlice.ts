c1a8a9605e4f8c28a48a76fae16a4e257eac1b82
ali kiamarsi <alikiamarsi74@gmail.com>
2026-07-09 20:48:35 +0300

    setup cardslice and add to card reducer

--- pclove-shop/store/cartSlice.ts
+++ pclove-shop/store/cartSlice.ts
@@ -1,7 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { CartItem } from "@/types/cart";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Product } from "@/types/product";
 
 interface CartState {
-    items: [];
+    items: CartItem[];
 }
 
 const initialState: CartState = {
@@ -11,8 +13,31 @@ const initialState: CartState = {
 const cartSlice = createSlice({
     name: "cart",
     initialState,
-    reducers: {},
+    reducers: {
+        addToCart: (
+            state,
+            action: PayloadAction<Product>
+        ) => {
+            const product = action.payload;
+            const existingItem = state.items.find(
+                (item) => item.id === product.id
+            );
+
+            if(existingItem) {
+                existingItem.quantity += 1;
+            } else {
+                state.items.push({
+                    id: product.id,
+                    title: product.title,
+                    price: product.price,
+                    image: product.image,
+                    quantity: 1,
+                })
+            }
+        }
+    },
 
 })
 
+export const {addToCart} = cartSlice.actions
 export default cartSlice.reducer;
