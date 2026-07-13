efff26d7413ad3e51c20efaf8cce197ebed6fe29
ali kiamarsi <alikiamarsi74@gmail.com>
2026-07-13 20:59:43 +0300

    feat: add cart quantity and remove actions

--- pclove-shop/store/cartSlice.ts
+++ pclove-shop/store/cartSlice.ts
@@ -34,10 +34,49 @@ const cartSlice = createSlice({
                     quantity: 1,
                 })
             }
+        },
+
+        increaseQuantity: (
+            state,
+            action: PayloadAction<number>
+        ) => {
+            const item = state.items.find(
+                (item) => item.id === action.payload
+            );
+            if(item){
+                item.quantity += 1;
+            }
+        },
+
+        decreaseQuantity: (
+            state,
+            action: PayloadAction<number>
+        ) => {
+            const item = state.items.find(
+                (item) => item.id === action.payload
+            );
+
+            if(item && item.quantity > 1) {
+                item.quantity -= 1;
+            }
+        },
+
+        removeFromCart: (
+            state,
+            action: PayloadAction<number>
+        ) => {
+            state.items = state.items.filter(
+                (item) => item.id !== action.payload
+            )
         }
     },
 
 })
 
-export const {addToCart} = cartSlice.actions
+export const {
+    addToCart,
+    increaseQuantity,
+    decreaseQuantity,
+    removeFromCart,
+} = cartSlice.actions
 export default cartSlice.reducer;
