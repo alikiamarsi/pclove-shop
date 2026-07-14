e8f428290fd3ea0945935cbbc974772031f1c0c1
ali kiamarsi <alikiamarsi74@gmail.com>
2026-07-14 21:01:19 +0300

    feat: add category dropdown and product filtering

--- pclove-shop/app/page.tsx
+++ pclove-shop/app/page.tsx
@@ -1,28 +1,46 @@
 import ProductCard from "@/components/ProductCard";
 import { Product } from "@/types/product";
 
-async function getProducts(): Promise<Product[]> {
-  const res = await fetch("http://localhost:5000/products", {
+type PageProps = {
+  searchParams: Promise<{
+    category?: string;
+  }>;
+};
+
+async function getProducts(category?: string): Promise<Product[]> {
+  const url = category
+    ? `http://localhost:5000/products?category=${category}`
+    : "http://localhost:5000/products";
+
+  const res = await fetch(url, {
     cache: "no-store",
-  })
-  if(!res.ok) {
+  });
+  if (!res.ok) {
     throw new Error("Failed to fetch products");
   }
   return res.json();
 }
 
- async function Home() {
-  const products = await getProducts();
+async function Home({ searchParams }: PageProps) {
+  const { category } = await searchParams;
+
+  const products = await getProducts(category);
   return (
     <main className="mx-auto max-w-7xl px-6 py-10">
-      <h1 className="mb-8 text-3xl font-bold">Product</h1>
+      <h1 className="mb-8 text-3xl font-bold">{category || "Products"}</h1>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {products.map((product) => (
+        {products.length === 0 ? (
+          <p className="col-span-full text-center text-gray-500">
+            No products found
+          </p>
+        ): (
+          products.map((product) => (
           <ProductCard key={product.id} product={product} />
-        ))}
+        ))
+        )}
       </div>
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
